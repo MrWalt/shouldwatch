01cdbda2b218d11e39a6e7dc0679855d46b2bf10
MrWalt <mihaelkrstevski.2002@yahoo.com>
2024-01-25 18:47:26 +0100

    Refactor footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MoviesList from "./components/MoviesList";
 import Summary from "./components/Summary";
 import WatchLaterList from "./components/WatchLaterList";
 import MovieDetails from "./components/MovieDetails";
+import Footer from "./components/Footer";
 
 export const API_KEY = "ad8548a2";
 
@@ -139,47 +140,5 @@ export default function App() {
       </main>
       <Footer />
     </>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="footer">
-      <span className="footer__text">Built by </span>
-      <a href="https://github.com/MrWalt/shouldwatch" className="footer__link">
-        MrWhite
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 256 256"
-          className="footer__icon"
-        >
-          <rect width="256" height="256" fill="none" />
-          <path
-            d="M119.83,56A52,52,0,0,0,76,32a51.92,51.92,0,0,0-3.49,44.7A49.28,49.28,0,0,0,64,104v8a48,48,0,0,0,48,48h48a48,48,0,0,0,48-48v-8a49.28,49.28,0,0,0-8.51-27.3A51.92,51.92,0,0,0,196,32a52,52,0,0,0-43.83,24Z"
-            fill="none"
-            stroke="currentColor"
-            strokLinecap="round"
-            strokLinejoin="round"
-            strokeWidth="16"
-          />
-          <path
-            d="M104,232V192a32,32,0,0,1,32-32h0a32,32,0,0,1,32,32v40"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="16"
-          />
-          <path
-            d="M104,208H72a32,32,0,0,1-32-32A32,32,0,0,0,8,144"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="16"
-          />
-        </svg>
-      </a>
-    </footer>
   );
 }
