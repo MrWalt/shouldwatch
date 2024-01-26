422199e679191aaedd5b3289ea10b53323d16dc8
MrWalt <mihaelkrstevski.2002@yahoo.com>
2024-01-26 09:08:16 +0100

    Disable input zoom on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,8 +14,8 @@ export default function Footer() {
             d="M119.83,56A52,52,0,0,0,76,32a51.92,51.92,0,0,0-3.49,44.7A49.28,49.28,0,0,0,64,104v8a48,48,0,0,0,48,48h48a48,48,0,0,0,48-48v-8a49.28,49.28,0,0,0-8.51-27.3A51.92,51.92,0,0,0,196,32a52,52,0,0,0-43.83,24Z"
             fill="none"
             stroke="currentColor"
-            strokLinecap="round"
-            strokLinejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             strokeWidth="16"
           />
           <path
@@ -30,9 +30,9 @@ export default function Footer() {
             d="M104,208H72a32,32,0,0,1-32-32A32,32,0,0,0,8,144"
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="16"
           />
         </svg>
       </a>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,16 @@
 export default function Movie({ movie, onSelectMovie }) {
   return (
-    <div className="movie" onClick={() => onSelectMovie(movie.imdbID)}>
-      <img src={movie.Poster} alt={movie.Title} className="movie__image" />
-      <div className="movie__info">
-        <p className="movie__title">{movie.Title}</p>
-        <span className="movie__released">{movie.Year}</span>
-        {movie.added && (
-          <span className="movie__added">Added {movie.added}</span>
-        )}
+    <a className="movie__link" href="#later">
+      <div className="movie" onClick={() => onSelectMovie(movie.imdbID)}>
+        <img src={movie.Poster} alt={movie.Title} className="movie__image" />
+        <div className="movie__info">
+          <p className="movie__title">{movie.Title}</p>
+          <span className="movie__released">{movie.Year}</span>
+          {movie.added && (
+            <span className="movie__added">Added {movie.added}</span>
+          )}
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
