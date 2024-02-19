d185c23e35cdb800dcafb142c2b44000fd51d2b3
MrWalt <mihaelkrstevski.2002@yahoo.com>
2024-02-19 21:59:29 +0100

    Add Search Results text and fix border bug

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,3 +1,10 @@
 export default function Box({ className, children }) {
-  return <div className={className}>{children}</div>;
+  return (
+    <div className="results__container">
+      {className === "results" && (
+        <span className="results__heading">Search Results</span>
+      )}
+      <div className={className}>{children}</div>
+    </div>
+  );
 }
